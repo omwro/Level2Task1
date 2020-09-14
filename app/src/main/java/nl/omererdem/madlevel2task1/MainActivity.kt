2d82f91d27ab5db61f10c168a0017a160a12dd24
Omer Erdem <m.o.erdem@outlook.com>
2020-09-14 10:45:11 +0200

    #0 Code optimization

--- app/src/main/java/nl/omererdem/madlevel2task1/MainActivity.kt
+++ app/src/main/java/nl/omererdem/madlevel2task1/MainActivity.kt
@@ -2,14 +2,20 @@ package nl.omererdem.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import nl.omererdem.madlevel2task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    var places= arrayListOf<Place>()
-    var placeAdapter = PlaceAdapter(places)
+    // Frontend binding var
+    private lateinit var binding: ActivityMainBinding
+
+    // List of places
+    private val places: ArrayList<Place> = arrayListOf()
+
+    // places set to a placeAdapter
+    private val placeAdapter = PlaceAdapter(places)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +24,20 @@ class MainActivity : AppCompatActivity() {
         initViews()
     }
 
-    fun initViews() {
+    private fun initViews() {
+        // Set staggered layout to recyclerview
         binding.rvPlaces.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
         binding.rvPlaces.adapter = placeAdapter
 
+        // Insert place names and images to the list
         for (i in Place.PLACE_NAMES.indices) {
-            places.add(Place(Place.PLACE_NAMES[i],
-                Place.PLACE_RES_DRAWABLE_IDS[i]))
+            places.add(
+                Place(Place.PLACE_NAMES[i],
+                Place.PLACE_RES_DRAWABLE_IDS[i])
+            )
         }
+
+        // Notify system changes has been made
         placeAdapter.notifyDataSetChanged()
     }
 }
